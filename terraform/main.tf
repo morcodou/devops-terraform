b2b4b3f1408c3b45a92a40e961ef09f388acf1bc
Mor Codou GUEYE <morcodou@gmail.com>
2021-05-13 16:14:06 -0400

    update format

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
+  version                    = "~>2.0"
   skip_provider_registration = true
   features {}
 }
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "tfdevopsgithubsrg" {
  
 #Create Virtual Network
 resource "azurerm_virtual_network" "tfdevopsgithubvnet" {
-  name                = "tfdevops-github-vnet"
+  name                = "myvnet"
   address_space       = ["192.168.0.0/16"]
   location            = azurerm_resource_group.tfdevopsgithubsrg.location
   resource_group_name = azurerm_resource_group.tfdevopsgithubsrg.name
@@ -33,7 +33,7 @@ resource "azurerm_virtual_network" "tfdevopsgithubvnet" {
  
 # Create Subnet
 resource "azurerm_subnet" "tfdevopsgithubsubnet" {
-  name                 = "tfdevopsgithubsubnet"
+  name                 = "mysubnet"
   resource_group_name  = azurerm_resource_group.tfdevopsgithubsrg.name
   virtual_network_name = azurerm_virtual_network.tfdevopsgithubvnet.name
   address_prefix       = "192.168.0.0/24"
